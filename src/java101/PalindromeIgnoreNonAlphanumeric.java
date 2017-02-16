920825bc9d45500cefa22d6229242a1e61ccc058
Yanru Mao <Yanru Mao>
2017-02-16 14:41:55 +0900

    0216

--- src/java101/PalindromeIgnoreNonAlphanumeric.java
+++ src/java101/PalindromeIgnoreNonAlphanumeric.java
@@ -17,12 +17,25 @@ public class PalindromeIgnoreNonAlphanumeric {
 	
 	public static boolean isPalindrome(String s){
 		String s1=filter(s);
-		String s2=filter(s1);
+		String s2=reverse(s1);
 		//reverse
 		return s2.equals(s1);
 	}
 	
 	public static String filter(String s){
+		StringBuilder stringBuilder = new StringBuilder();
 		
+		for(int i=0;i<s.length();i++){
+			if(Character.isLetterOrDigit(s.charAt(i))){
+				stringBuilder.append(s.charAt(i));
+			}
+		}
+		return stringBuilder.toString();
+	}
+	
+	public static String reverse(String s){
+		StringBuilder stringBuilder = new StringBuilder(s);
+		stringBuilder.reverse();
+		return stringBuilder.toString();
 	}
 }
