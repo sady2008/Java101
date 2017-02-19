f427f2d46ed3b9bdc47c63654e01839ccfd5ce40
Yanru Mao <Yanru Mao>
2017-02-19 14:42:01 +0900

    0219

---
+++ src/java101/TestArraryList.java
@@ -0,0 +1,13 @@
+package java101;
+
+import java.util.ArrayList;
+
+public class TestArraryList {
+	public static void main(String[] args){
+		ArrayList<String> cityList = new ArrayList<>();
+		//Create a List
+		cityList.add("London");
+		cityList.add("New York");
+	}
+
+}
